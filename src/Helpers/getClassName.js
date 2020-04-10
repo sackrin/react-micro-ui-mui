1a50af9ca3b6525246505b8b1001f943cb1d5a9f
Hilary Osborne <h.t.osborne@gmail.com>
2020-04-10 14:06:53 +1000

    Delete getClassName.js

--- src/Helpers/getClassName.js
+++
@@ -1,7 +0,0 @@
-import shortId from 'shortid';
-import { createGenerateClassName } from '@material-ui/core/styles';
-const getClassName = productionPrefix => createGenerateClassName({
-    productionPrefix,
-    seed: shortId(),
-});
-export default getClassName;
